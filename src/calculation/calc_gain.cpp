861e76fdcbc59f43f125cf859806d2f8943c6478
松岡 昇 <matsuoka@matsuokanoMacBook-Air.local>
2025-05-27 16:26:33 +0900

    changed calc_ripple.cpp

--- src/calculation/calc_gain.cpp
+++ src/calculation/calc_gain.cpp
@@ -18,11 +18,11 @@ double calc_gain(vector<vector<double>> csv_array, double freq_r) {
 
     double gain = csv_array[0][1];
     double freq = freq_r - 0.5; // resonator freq kara 0.5GHz hikui tokoro
-    double width = 1.5; // tansaku no haba
+    //double width = 1.5; // tansaku no haba
 
     for (auto& column : csv_array) {
-        if(column[0] >= freq - width && column[0] <= freq){ // hani wo resonator freq kara 0.5GHz hikui tokoro kara 1.5GHz no haba no aida nomi wo tansaku
-            if(column[1] > gain){
+        if(column[0] >= freq - WIDTH_F && column[0] <= freq){  //範囲を共振周波数から0.5 GHz低いところから1.5GHzの幅の間のみを探索
+            if(column[1] > gain){                            
                 gain = column[1];
             } 
         }
